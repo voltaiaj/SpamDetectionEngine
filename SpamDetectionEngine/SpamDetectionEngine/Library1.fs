1e8080bcd9067fda7dedba3e64a98e1fbc727f4c
voltaiaj <alexander.voltaire@gmail.com>
2015-09-03 19:58:40 -0500

    Starting project

---
+++ SpamDetectionEngine/SpamDetectionEngine/Library1.fs
@@ -0,0 +1,4 @@
+﻿namespace SpamDetectionEngine
+
+type Class1() = 
+    member this.X = "F#"
---
+++ SpamDetectionEngine/SpamDetectionEngine/Script.fsx
@@ -0,0 +1,13 @@
+﻿type DocType = 
+    | Ham
+    | Spam
+
+
+let identify (example:DocType*string) = 
+    let docType,content = example
+    match docType with
+    | Ham -> printfn "'%s' is ham" content
+    | Spam -> printfn "'%s' is spam" content
+
+identify (Ham, "good message");;
+    

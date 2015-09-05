4bd04f3d657656d122478e0ff6ef7e1eb99e94ec
voltaiaj <alexander.voltaire@gmail.com>
2015-09-04 22:30:02 -0500

    Adding SMSSpamCollection

--- SpamDetectionEngine/SpamDetectionEngine/Script.fsx
+++ SpamDetectionEngine/SpamDetectionEngine/Script.fsx
@@ -1,13 +1,51 @@
-﻿type DocType = 
+﻿open System.IO
+
+type DocType = 
     | Ham
     | Spam
 
+let parseDocType (label:string) =
+    match label with
+    | "ham"  -> Ham
+    | "spam" -> Spam
+    | _      -> failwith "Unknown label"
 
-let identify (example:DocType*string) = 
-    let docType,content = example
-    match docType with
-    | Ham -> printfn "'%s' is ham" content
-    | Spam -> printfn "'%s' is spam" content
+let parseLine (line:string) = 
+    let split = line.Split('\t')
+    let label = split.[0] |> parseDocType
+    let message = split.[1]
+    (label, message)
 
-identify (Ham, "good message");;
+let fileName = "SMSSpamCollection"
+let path = __SOURCE_DIRECTORY__ + @"..\..\Data\" + fileName
+
+let dataset = 
+    File.ReadAllLines path
+    |> Array.map parseLine
+
+let spamWithFREE = 
+    dataset 
+    |> Array.filter (fun (docType,_) -> docType = Spam)
+    |> Array.filter (fun (_,sms) -> sms.Contains("FREE"))
+    |> Array.length
+
+let hamWithFREE = 
+    dataset
+    |> Array.filter (fun (docType,_) -> docType = Ham)
+    |> Array.filter (fun (_,sms) -> sms.Contains("FREE"))
+    |> Array.length
+
+let primitiveClassifier (sms:string) = 
+    if (sms.Contains "FREE")
+    then Spam
+    else Ham
+
+
+//let identify (example:DocType*string) = 
+//    let docType,content = example
+//    match docType with
+//    | Ham -> printfn "'%s' is ham" content
+//    | Spam -> printfn "'%s' is spam" content
+//
+//identify (Ham, "good message");;
     
